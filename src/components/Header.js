8d831e4e2e7f1b8855ab772765247cd67b0cb463
Christine Wong <jumiryoo@gmail.com>
2023-07-18 19:09:51 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+
+function Header() {
+    return (
+        <ul>
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#github">Github</a></li>
+            <li><a href="#linkedin">LinkedIn</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Projects from './Projects';
+
+// the component function
+export default function Main(props) {
+
+    //The Components Returned JSX
+    return <div className="Main"> <Projects/></div>
+}
+
+// export the component
+export default Main;
